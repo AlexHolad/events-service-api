43f4d753fe327721a8dc8a06da3724cffc47f132
alexholad <alexej.holad@gmail.com>
2023-12-01 20:08:26 +0100

    fixed bug with signin

--- controllers/signin.controller.js
+++ controllers/signin.controller.js
@@ -1,13 +1,16 @@
-import  User  from "../models/User.js";
+import User from "../models/User.js";
 
-import { generateAccessToken, generateRefreshToken } from "./Auth/auth.controller.js";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "./Auth/auth.controller.js";
 
 export const handleSignIn = (bcrypt) => async (req, res) => {
   const { email, password } = req.body;
-
+  console.log(req.body);
   // VALIDATION
   if (!email || !password) {
-    return res.status(400).json("incorrect form submission");
+    return res.status(400).json({ message: "Введите логин и пароль" });
   }
 
   try {
@@ -15,29 +18,33 @@ export const handleSignIn = (bcrypt) => async (req, res) => {
     const foundUser = await User.findOne({ email: email }).exec();
 
     if (!foundUser) {
-      return res.status(401).json({ message: 'Unathorized' })
+      return res
+        .status(401)
+        .json({ message: "Аккаунт с таким email не существует" });
     }
 
     // Compare hashed password with password from client
     const match = await bcrypt.compare(password, foundUser.password);
-
-    if(!match) return res.sendStatus(400).json("wrong credentials");
-
-    const accessToken = generateAccessToken(foundUser);
-    const refreshToken = generateRefreshToken(foundUser);
+    console.log(match)
     
-    // Create secure cookie with refresh token
-    res.cookie('jwt', refreshToken, {
-      httpOnly: true, // accessible only by web browser
-      secure: true, // https
-      sameSite: 'None', // cross-site cookie
-      maxAge: 7 * 24 * 60 * 60 * 1000 // cookie expiry: set to match rT
-    })
-
-    // Send token containing email and role
-    res.json({accessToken});
-
-  } catch (err) {
-    res.status(400).json("wrong credentials");
+    if (!match) {
+      return res.status(400).json({ message: "Неверная комбинация логина и пароля" });
+    } 
+    
+      const accessToken = generateAccessToken(foundUser);
+      const refreshToken = generateRefreshToken(foundUser);
+  
+      // Create secure cookie with refresh token
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true, // accessible only by web browser
+        secure: true, // https
+        sameSite: "None", // cross-site cookie
+        maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expiry: set to match rT
+      });
+  
+      // Send token containing email and role
+      return res.json({ accessToken });
+ } catch (err) {
+    res.status(400).json({ message: "wrong credentials" });
   }
-};
+}
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -19,4 +19,5 @@ export const verifyJWT = (req, res, next) => {
             next()  
         }
     );
+
   };
